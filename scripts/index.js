0320a1d6f02db5c76de214dbd0af7b9ff801fd02
matth3wn <matth3wn@yahoo.com>
2019-01-28 09:45:30 -0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,14 @@ $(document).ready(function () {
 
 store.items.push(Item.create('apples'));
 
-api.createItem('pears')
-  .then(res => res.json())
-  .then((newItem) => {
-    return api.getItems();
-  })
-  .then(res => res.json())
-  .then(items => {
-    console.log(items);
-  });
 
+api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    const item = items[0];
+    return api.updateItem(item.id, { name: 'foobar' });
+  })
+  .then(() => {
+    // we don't care about the response body here so we don't need to run res.json()
+     console.log('updated!');
+  });
