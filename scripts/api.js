2526112dcacc79ef53f0b25c5645d6164ef16405
matth3wn <matth3wn@yahoo.com>
2019-01-28 09:06:05 -0800

    Update api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,26 @@
-const api = (function (){
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mattRobin';
-    const getItems = function (){
-      return fetch(`${BASE_URL}/items`);
+'use strict';
+
+const api = (function () {
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/matt';
+  const getItems = function () {
+    return fetch(`${this.BASE_URL}/items`);
+  };
+
+  const createItem = function(name){
+    const newItem = {
+      name: name,
     };
-    return {
-        getItems,
+
+    const options ={
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(newItem),
     }
+
+    return fetch(`${BASE_URL}/items`,options);
+
+  };
+  return {
+    getItems,createItem
+  }
 }());
