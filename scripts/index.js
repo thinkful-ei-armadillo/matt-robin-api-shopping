2854caeb5dcee459515f27e18da267743dcb90c4
Robin Khiv <robinkhiv@gmail.com>
2019-01-28 11:26:55 -0800

    findAndUpdate works

--- scripts/index.js
+++ scripts/index.js
@@ -9,13 +9,7 @@ $(document).ready(function () {
 store.items.push(Item.create('apples'));
 
 
-api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    const item = items[0];
-    return api.updateItem(item.id, { name: 'foobar' });
-  })
-  .then(() => {
-    // we don't care about the response body here so we don't need to run res.json()
-     console.log('updated!');
-  });
+const item = store.items[0];
+console.log('current name: ' + item.name);
+store.findAndUpdate(item.id, { name: 'foobar' });
+console.log('new name: ' + item.name);
