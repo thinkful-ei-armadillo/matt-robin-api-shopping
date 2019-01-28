236b3c0a7305869118c9124eec2e065e2e8ceab5
Robin Khiv <robinkhiv@gmail.com>
2019-01-28 08:44:54 -0800

    getitem fetch works

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,7 @@
 const api = (function (){
     const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mattRobin';
     const getItems = function (){
-      return fetch(`${this.BASE_URL}/items`);
+      return fetch(`${BASE_URL}/items`);
     };
     return {
         getItems,
