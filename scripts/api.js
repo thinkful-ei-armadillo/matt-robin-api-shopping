146fe91f2ac55c7b8f4d01b31ddc5cd066726780
matth3wn <matth3wn@yahoo.com>
2019-01-28 09:45:11 -0800

    update

--- scripts/api.js
+++ scripts/api.js
@@ -6,22 +6,36 @@ const api = (function () {
     return fetch(`${BASE_URL}/items`);
   };
 
-  const createItem = function(name){
+  const createItem = function (name) {
     const newItem = {
       name: name,
     };
 
-    const options ={
+    const options = {
       method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
       body: JSON.stringify(newItem),
     }
 
-    return fetch(`${BASE_URL}/items`,options);
+    return fetch(`${BASE_URL}/items`, options);
 
+  };
+  const updateItem = function (id, updateData) {
+    const options = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(updateData),
+    };
+
+    return fetch(`${BASE_URL}/items/${id}`,options);
   };
   return {
     getItems,
-    createItem
+    createItem,
+    updateItem,
   }
 }());
